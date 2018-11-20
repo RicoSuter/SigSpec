679a8ec5508d94d874be14017d34047c1337b8bd
Janne Siera <janne.siera@gmail.com>
2018-11-20 21:54:28 +0100

    Don't use lazy, for simplicity's sake

--- src/SigSpec.CodeGeneration/Models/ParameterModel.cs
+++ src/SigSpec.CodeGeneration/Models/ParameterModel.cs
@@ -10,18 +10,18 @@ namespace SigSpec.CodeGeneration.Models
         private readonly string _name;
         private readonly SigSpecParameter _parameter;
         private readonly TypeResolverBase _resolver;
-        private readonly Lazy<string> _type;
+        private readonly string _type;
 
         public ParameterModel(string name, SigSpecParameter parameter, TypeResolverBase resolver)
         {
             _name = name;
             _parameter = parameter;
             _resolver = resolver;
-            _type = new Lazy<string>(() => _resolver.Resolve(_parameter.ActualTypeSchema, _parameter.IsNullable(SchemaType.JsonSchema), _name));
+            _type = _resolver.Resolve(_parameter.ActualTypeSchema, _parameter.IsNullable(SchemaType.JsonSchema), _name);
         }
 
         public string Name => _name;
 
-        public string Type => _type.Value;
+        public string Type => _type;
     }
 }
