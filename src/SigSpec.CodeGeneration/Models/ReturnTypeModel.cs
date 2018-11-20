12a762a5691205f46895573caf33e59412202565
Janne Siera <janne.siera@gmail.com>
2018-11-20 10:57:14 +0100

    Cache type evaluation in model

--- src/SigSpec.CodeGeneration/Models/ReturnTypeModel.cs
+++ src/SigSpec.CodeGeneration/Models/ReturnTypeModel.cs
@@ -1,6 +1,7 @@
 ﻿using NJsonSchema;
 using NJsonSchema.CodeGeneration;
 using SigSpec.Core;
+using System;
 
 namespace SigSpec.CodeGeneration.Models
 {
@@ -9,16 +10,18 @@ namespace SigSpec.CodeGeneration.Models
         private readonly string _name;
         private readonly JsonSchema4 _parameter;
         private readonly TypeResolverBase _resolver;
+        private readonly Lazy<string> _type;
 
         public ReturnTypeModel(string name, JsonSchema4 parameter, TypeResolverBase resolver)
         {
             _name = name;
             _parameter = parameter;
             _resolver = resolver;
+            _type = new Lazy<string>(() => _resolver.Resolve(_parameter.ActualTypeSchema, _parameter.IsNullable(SchemaType.JsonSchema), _name));
         }
 
         public string Name => _name;
 
-        public string Type => _resolver.Resolve(_parameter.ActualTypeSchema, _parameter.IsNullable(SchemaType.JsonSchema), _name);
+        public string Type => _type.Value;
     }
 }
