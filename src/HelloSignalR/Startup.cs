66a6a0443bee565d93fdfc381c046f8e8d062d3c
Bela Orosz <bela.orosz@gmail.com>
2020-02-14 20:51:41 +0100

    HubConnection unregister bugfix

--- src/HelloSignalR/Startup.cs
+++ src/HelloSignalR/Startup.cs
@@ -6,14 +6,29 @@ namespace HelloSignalR
 {
     public class Startup
     {
+        readonly string AllowAllPolicy = "AllowAllPolicy";
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(AllowAllPolicy,
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowAnyOrigin()
+                            .AllowCredentials();
+                    });
+            });
             services.AddSignalR();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseStaticFiles();
+            app.UseCors(AllowAllPolicy);
             app.UseSignalR(routes =>
             {
                 routes.MapHub<ChatHub>("/chat");
