78db800bee88b6786ccf0a5114aaba2e1bb05067
TSmith <tom.smith2711@gmail.com>
2020-05-26 00:07:30 +0100

    remove tests to make project simpler

--- src/SigSpec.UI/.eslintrc.js
+++ src/SigSpec.UI/.eslintrc.js
@@ -1,23 +1,15 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/typescript/recommended'],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-  overrides: [
-    {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
 };
