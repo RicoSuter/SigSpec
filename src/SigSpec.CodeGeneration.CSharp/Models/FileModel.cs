f35a60b59de5462871f4dfd4025c92cc068e52d7
Dave <david.beiler@georgfischer.com>
2020-12-16 16:24:15 -0600

    producing working c# clients

--- src/SigSpec.CodeGeneration.CSharp/Models/FileModel.cs
+++ src/SigSpec.CodeGeneration.CSharp/Models/FileModel.cs
@@ -4,10 +4,12 @@ namespace SigSpec.CodeGeneration.CSharp.Models
 {
     public class FileModel
     {
-        public FileModel(IEnumerable<string> hubs)
+        public FileModel(IEnumerable<string> hubs, string @namespace)
         {
             Hubs = hubs;
+            Namespace = @namespace;
         }
+        public string Namespace { get; set; }
 
         public IEnumerable<string> Hubs { get; }
     }
