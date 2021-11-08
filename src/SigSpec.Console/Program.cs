70bcded7d0a602aab35d40caa1e54e9a9e260a67
Ryan Jones <rak.jones.94@gmail.com>
2021-11-09 09:52:53 +1300

    Generate both C# and TypeScript clients in console

--- src/SigSpec.Console/Program.cs
+++ src/SigSpec.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using HelloSignalR;
 using SigSpec.CodeGeneration.CSharp;
+using SigSpec.CodeGeneration.TypeScript;
 using SigSpec.Core;
 
 namespace SigSpec
@@ -31,9 +32,17 @@ namespace SigSpec
             Console.WriteLine(json);
             Console.ReadKey();
 
+            var tsCodeGeneratorSettings = new SigSpecToTypeScriptGeneratorSettings();
+            var tsCodeGenerator = new SigSpecToTypeScriptGenerator(tsCodeGeneratorSettings);
+            var file = tsCodeGenerator.GenerateFile(document);
+
+            Console.WriteLine("\n\nGenerated SigSpec TypeScript code:");
+            Console.WriteLine(file);
+            Console.ReadKey();
+
             var codeGeneratorSettings = new SigSpecToCSharpGeneratorSettings();
             var codeGenerator = new SigSpecToCSharpGenerator(codeGeneratorSettings);
-            var file = codeGenerator.GenerateClients(document);
+            file = codeGenerator.GenerateClients(document);
 
             Console.WriteLine("\n\nGenerated SigSpec CSharp clients:");
             Console.WriteLine(file);
