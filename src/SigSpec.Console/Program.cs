0a4682612145635f12c0a1d8af131caae0ef4287
Vincent Chartier <vincent.chartier@raycowylie.com>
2023-07-31 13:09:05 -0400

    Ajout des fonctionnalité permettant d'appeler la librairie de façon indépendante en utilisant la réflexion.

--- src/SigSpec.Console/Program.cs
+++ src/SigSpec.Console/Program.cs
@@ -1,52 +1,295 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.IO;
 using System.Threading.Tasks;
-using HelloSignalR;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 using SigSpec.CodeGeneration.CSharp;
 using SigSpec.CodeGeneration.TypeScript;
 using SigSpec.Core;
 
 namespace SigSpec
 {
-    class Program
+    static class Program
     {
+        private static string? csharpFile = null;
+        private static string? jsonFile = null;
+        private static string? typescriptFile = null;
+        private static string? targetAssemblyName = null;
+        
+        private static string? targetFile = null;
+        
+        
+        private static string? assemblyNamespace = null;
+
+        
+            
         static void Main(string[] args)
         {
-            Console.WriteLine("SigSpec for SignalR Core");
-            RunAsync().GetAwaiter().GetResult();
-        }
+            ProcessArgs(args);
+            
+            
+            string dllname = Path.GetFileNameWithoutExtension(targetAssemblyName);
+            string currentDir = Directory.GetCurrentDirectory();
 
-        static async Task RunAsync()
-        {
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(targetFile)!);
+            
+            LoadAssemblies();
+            
+            Assembly targetAss = AppDomain.CurrentDomain.GetAssemblies().First(a => a.FullName.StartsWith(dllname));
+            
+                        
+            
             var settings = new SigSpecGeneratorSettings();
+            settings.UseXmlDocumentation = true;
+            
+            
             var generator = new SigSpecGenerator(settings);
+            
+            
+            Type[] types = targetAss.GetTypes();
+            
+            Type[] hubs = types.Where(t => t.BaseType?.FullName?.Contains("Microsoft.AspNetCore.SignalR.Hub") ?? false).ToArray();
 
-            // TODO: Add PR to SignalR Core with new IHubDescriptionCollectionProvider service
-            var document = await generator.GenerateForHubsAsync(new Dictionary<string, Type>
+            var document = generator.GenerateForHubsAsync(new Dictionary<string, Type>(hubs.Select(t => new KeyValuePair<string, Type>(t.Name, t)))).Result;
+
+            string? pathXml = Namotion.Reflection.XmlDocsExtensions.GetXmlDocsPath(targetAss, true);
+
+            Directory.SetCurrentDirectory(currentDir);
+            
+            
+            Console.WriteLine(pathXml);
+            
+            
+            //On va chercher RSimplified et on retire les champs qui ne sont pas pertinent.
+            var rSimplified = document.Definitions["RSimplified"];
+            foreach (var key in rSimplified.Properties.Keys)
             {
-                { "chat", typeof(ChatHub) }
-            });
+                if (key != "stringID" && key != "properties" && key != "objectValue")
+                    rSimplified.Properties.Remove(key);
+            }
+
+            RemoveLong(document.Definitions["DiagSensor"]);
+            RemoveLong(document.Definitions["DiagItem"]);
 
             var json = document.ToJson();
-            Console.WriteLine("\nGenerated SigSpec document:");
-            Console.WriteLine(json);
-            Console.ReadKey();
+            if(jsonFile is null)
+                Console.WriteLine(json);
+            else
+                File.WriteAllText(jsonFile, json, Encoding.UTF8);
 
-            var tsCodeGeneratorSettings = new SigSpecToTypeScriptGeneratorSettings();
-            var tsCodeGenerator = new SigSpecToTypeScriptGenerator(tsCodeGeneratorSettings);
-            var file = tsCodeGenerator.GenerateFile(document);
+            if (typescriptFile is not null)
+            {
+                var tsCodeGeneratorSettings = new SigSpecToTypeScriptGeneratorSettings();
+                var tsCodeGenerator = new SigSpecToTypeScriptGenerator(tsCodeGeneratorSettings);
+                var file = tsCodeGenerator.GenerateFile(document);
 
-            Console.WriteLine("\n\nGenerated SigSpec TypeScript code:");
-            Console.WriteLine(file);
-            Console.ReadKey();
+                File.WriteAllText(typescriptFile, file);
+            }
 
-            var codeGeneratorSettings = new SigSpecToCSharpGeneratorSettings();
-            var codeGenerator = new SigSpecToCSharpGenerator(codeGeneratorSettings);
-            file = codeGenerator.GenerateClients(document);
+            if (csharpFile is not null)
+            {
+                var codeGeneratorSettings = new SigSpecToCSharpGeneratorSettings();
+                if (assemblyNamespace is not null)
+                    codeGeneratorSettings.CSharpGeneratorSettings.Namespace = assemblyNamespace;
+                var codeGenerator = new SigSpecToCSharpGenerator(codeGeneratorSettings);
+                var file = codeGenerator.GenerateClients(document);
 
-            Console.WriteLine("\n\nGenerated SigSpec CSharp clients:");
-            Console.WriteLine(file);
-            Console.ReadKey();
+                File.WriteAllText(csharpFile, file);
+            }
+        }
+
+        public static void ProcessArgs(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                DisplayHelp();
+                return;
+            }
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].StartsWith("--"))
+                {
+                    if (args[i].StartsWith("--help") && args.Length > i + 1)
+                    {
+                        DisplayHelp();
+                        return;
+                    }
+                    else if (args[i].StartsWith("--json") && args.Length > i + 1)
+                    {
+                        i++;
+                        jsonFile = args[i];
+                    }
+                    else if (args[i].StartsWith("--csharp") && args.Length > i + 1)
+                    {
+                        i++;
+                        csharpFile = args[i];
+                    }
+                    else if (args[i].StartsWith("--typescript"))
+                    {
+                        i++;
+                        typescriptFile = args[i];
+                    }
+                    else if (args[i].StartsWith("--assembly"))
+                    {
+                        i++;
+                        targetAssemblyName = args[i];
+                    }
+                    else if (args[i].StartsWith("--namespace"))
+                    {
+                        i++;
+                        assemblyNamespace = args[i];
+                    }
+                }
+                else if (args[i].StartsWith("-"))
+                {
+                    if (args[i].Contains('h') && args.Length > i + 1)
+                    {
+                        DisplayHelp();
+                        return;
+                    }
+                    else if (args[i].EndsWith('j') && args.Length > i + 1)
+                    {
+                        i++;
+                        jsonFile = args[i];
+                    }
+                    else if (args[i].EndsWith("c") && args.Length > i + 1)
+                    {
+                        i++;
+                        csharpFile = args[i];
+                    }
+                    else if (args[i].EndsWith("t") && args.Length > i + 1)
+                    {
+                        i++;
+                        typescriptFile = args[i];
+                    }
+                    else if (args[i].EndsWith("a") && args.Length > i + 1)
+                    {
+                        i++;
+                        targetAssemblyName = args[i];
+                    }
+                }
+                else
+                {
+                    targetFile = String.Join(" ", args.TakeLast(args.Length - i));
+                }
+            }
+            if(targetFile is null)
+                DisplayHelp(); 
+        }
+
+        private static void DisplayHelp()
+        {
+            Console.Error.WriteLine("Usage: sigspec [options] target" + Environment.NewLine +
+                                    "SigSpec is a tool building specifications documents for SignalR Core hubs." + Environment.NewLine +
+                                    "Options:" + Environment.NewLine +
+                                    "  -h, --help\t\t\tDisplay this help message." + Environment.NewLine +
+                                    "  -a, --assembly <file>\t\tSpecify the target assembly containing the SignalR Core hubs." + Environment.NewLine +
+                                    "  -j, --json <file>\t\tSpecify the output file for the SigSpec JSON document." + Environment.NewLine +
+                                    "  -c, --csharp <file>\t\tSpecify the output file for the C# clients." + Environment.NewLine +
+                                    "  -t, --typescript <file>\tSpecify the output file for the TypeScript clients." + Environment.NewLine +
+                                    "  --namespace <namespace>\tSpecify the namespace for the C# clients." + Environment.NewLine +
+                                    "" + Environment.NewLine +
+                                    "Arguments:" + Environment.NewLine +
+                                    "  target\t\t\t\tSpecify the library files to process. If no assembly option is specified, " + Environment.NewLine +
+                                    "        \t\t\t\tthe target name, without extension, is used." + Environment.NewLine +
+                                    "" + Environment.NewLine +
+                                    "Output:" + Environment.NewLine +
+                                    "  If no output option is specified(json,csharp or typescript), the json output is written to the console." + Environment.NewLine +
+                                    "" + Environment.NewLine +
+                                    "Examples:" + Environment.NewLine +
+                                    "  sigspec -j sigspec.json -c sigspec.cs -t sigspec.ts controller.dll" + Environment.NewLine +
+                                    "      Analyse controller.dll, using \"controller\" as the assembly name to search for SignalR Hubs." + Environment.NewLine +
+                                    "      Write the SigSpec JSON document to sigspec.json, the C# clients to sigspec.cs and the TypeScript clients to sigspec.ts." + Environment.NewLine +
+                                    "" + Environment.NewLine +
+                                    "  sigspec -a RaycoWylie.Server.FlatTopTC controller.dll" + Environment.NewLine +   
+                                    "      Analyse controller.dll, using \"RaycoWylie.Server.FlatTopTC\" as the assembly name to search for SignalR Hubs." + Environment.NewLine +
+                                    "      Write the SigSpec JSON document to the console.");
+
+        }
+
+        private static void LoadAssemblies()
+        {
+            string dllDir = Path.GetDirectoryName(targetFile)!;
+            string[] dlls = Directory.GetFiles(dllDir, "*.dll");
+
+            foreach (string dll in dlls)
+                AppDomain.CurrentDomain.Load(File.ReadAllBytes(dll));
+
+            foreach (string dll in Directory.GetFiles(Path.Join(dllDir, "win-x64"), "*.dll"))
+            {
+                try
+                {
+                    AppDomain.CurrentDomain.Load(File.ReadAllBytes(dll));
+                }
+                catch (BadImageFormatException e)
+                {
+                    Console.Error.WriteLine("Not loaded : " + dll);
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine("Not loaded : " + dll);
+                }
+            }
+
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, eventArgs) =>
+            {
+                Console.WriteLine(eventArgs.Name);
+                var asss = AppDomain.CurrentDomain.GetAssemblies();
+
+                Assembly? bob = asss.FirstOrDefault(a => a.ToString() == eventArgs.Name);
+                if (bob is not null)
+                {
+                    Console.WriteLine("Pre-Loaded : " + bob.GetName().ToString());
+                    return bob;
+                }
+
+                string assemblyPath = Path.Combine(dllDir, new AssemblyName(eventArgs.Name).Name + ".dll");
+                if (File.Exists(assemblyPath))
+                {
+                    Assembly ass = Assembly.LoadFrom(assemblyPath);
+                    Console.WriteLine("LOADED : " + ass.GetName().ToString());
+                    return ass;
+                }
+                assemblyPath = Path.Join( dllDir, "win-x64", new AssemblyName(eventArgs.Name).Name + ".dll"); 
+                if(File.Exists(assemblyPath))
+                {
+                    Assembly ass = Assembly.LoadFrom(assemblyPath);
+                    Console.WriteLine("LOADED : " + ass.GetName().ToString());
+                    return ass;
+                }
+                
+                string? pathVar = Environment.GetEnvironmentVariable("PATH");
+                string[] paths = pathVar?.Split(';') ?? new string[0];
+                foreach (string path in paths)
+                {
+                    assemblyPath = Path.Join( path, new AssemblyName(eventArgs.Name).Name + ".dll"); 
+                    if(File.Exists(assemblyPath))
+                    {
+                        Assembly ass = Assembly.LoadFrom(assemblyPath);
+                        Console.WriteLine("LOADED : " + ass.GetName().ToString());
+                        return ass;
+                    }
+                }
+                Console.WriteLine("Not found.");
+                return null;
+            };
+            
+        }
+        
+        private static void RemoveLong(NJsonSchema.JsonSchema schema)
+        {
+            foreach(var key in schema.Properties.Keys)
+            {
+                if(schema.Properties[key].Format == "int64")
+                {
+                    schema.Properties.Remove(key);
+                }
+            }
         }
     }
 }
--- src/test/Program.cs
+++ src/test/Program.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices;
 using System.Runtime.Loader;
 using Fluid.Filters;
 using Microsoft.AspNetCore.SignalR;
+using Namotion.Reflection;
 using SigSpec.CodeGeneration.CSharp;
 using SigSpec.CodeGeneration.TypeScript;
 using SigSpec.Core;
@@ -13,58 +14,85 @@ namespace test
     {
         static void Main(string[] args)
         {
+            
+            
             string dllFile = "D:\\workingdir\\newgen\\code\\git\\demo-comansa\\RaycoWylie.Server.FlatTopTC\\bin\\Release\\net6.0\\RaycoWylie.Server.FlatTopTC.dll";
             string dllname = Path.GetFileNameWithoutExtension(dllFile);
             string dllDir = Path.GetDirectoryName(dllFile)!;
             string[] dlls = Directory.GetFiles(dllDir, "*.dll");
-            string[] pdbs = Directory.GetFiles(dllDir, "*.pdb");
 
+            foreach (string dll in dlls)
+                AppDomain.CurrentDomain.Load(File.ReadAllBytes(dll));
 
-
-            //Assembly ServerFlatTop = Assembly.LoadFile("D:\\workingdir\\newgen\\code\\git\\demo-comansa\\RaycoWylie.Server.FlatTopTC\\bin\\Release\\net6.0\\RaycoWylie.Server.FlatTopTC.dll");
-            /*
-            AssemblyLoadContext loadContext = new AssemblyLoadContext(null); //System.Runtime.Loader.AssemblyLoadContext.GetLoadContext(ServerFlatTop);
-            //Assembly ServerFlatTop = loadContext.LoadFromAssemblyPath(dllFile);
-
-            
-            foreach(string dll in dlls)
-                loadContext.LoadFromAssemblyPath(dll);
-
-           *foreach (string dll in Directory.GetFiles(".", "*.dll"))
+            foreach (string dll in Directory.GetFiles(Path.Join(dllDir, "win-x64"), "*.dll"))
             {
-                if(loadContext.Assemblies.Count(a => loadContext.Assemblies.First().GetName().Name == Path.GetFileNameWithoutExtension(dll)) == 0 && dll != null)
-                    loadContext.LoadFromAssemblyPath(Path.GetFullPath(dll));
-            }(/
+                try
+                {
+                    AppDomain.CurrentDomain.Load(File.ReadAllBytes(dll));
+                }
+                catch (BadImageFormatException e)
+                {
+                    Console.WriteLine("Not loaded : " + dll);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Not loaded : " + dll);
+                }
+            }
 
-            Assembly ServerFlatTop = loadContext.Assemblies.First(a => a.FullName.StartsWith(dllname));
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, eventArgs) =>
+            {
+                Console.WriteLine(eventArgs.Name);
+                var asss = AppDomain.CurrentDomain.GetAssemblies();
 
+                Assembly? bob = asss.FirstOrDefault(a => a.ToString() == eventArgs.Name);
+                if (bob is not null)
+                {
+                    Console.WriteLine("Pre-Loaded : " + bob.GetName().ToString());
+                    return bob;
+                }
 
-            /*
-            AppDomain doms = AppDomain.CreateDomain("bob");
-
-            foreach(string dll in dlls)
-                doms.Load(File.ReadAllBytes(dll));
-
-
-            Assembly ServerFlatTop = doms.GetAssemblies().First(a => a.FullName.StartsWith(dllname));
-            */
-
-
-            PluginLoadContext context = new PluginLoadContext(dllFile);
-
-            Assembly ServerFlatTop = context.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(dllFile)));
-
-
-
-
+                string assemblyPath = Path.Combine(dllDir, new AssemblyName(eventArgs.Name).Name + ".dll");
+                if (File.Exists(assemblyPath))
+                {
+                    Assembly ass = Assembly.LoadFrom(assemblyPath);
+                    Console.WriteLine("LOADED : " + ass.GetName().ToString());
+                    return ass;
+                }
+                assemblyPath = Path.Join( dllDir, "win-x64", new AssemblyName(eventArgs.Name).Name + ".dll"); 
+                if(File.Exists(assemblyPath))
+                {
+                    Assembly ass = Assembly.LoadFrom(assemblyPath);
+                    Console.WriteLine("LOADED : " + ass.GetName().ToString());
+                    return ass;
+                }
+                
+                string? pathVar = Environment.GetEnvironmentVariable("PATH");
+                string[] paths = pathVar?.Split(';') ?? new string[0];
+                foreach (string path in paths)
+                {
+                    assemblyPath = Path.Join( path, new AssemblyName(eventArgs.Name).Name + ".dll"); 
+                    if(File.Exists(assemblyPath))
+                    {
+                        Assembly ass = Assembly.LoadFrom(assemblyPath);
+                        Console.WriteLine("LOADED : " + ass.GetName().ToString());
+                        return ass;
+                    }
+                }
+                Console.WriteLine("Not found.");
+                return null;
+            };
+            Assembly ServerFlatTop = AppDomain.CurrentDomain.GetAssemblies().First(a => a.FullName.StartsWith(dllname));
+            
+            
+            
             Type[] types = ServerFlatTop.GetTypes();
 
 
             var settings = new SigSpecGeneratorSettings();
             var generator = new SigSpecGenerator(settings);
 
-            Type[] hubss = types.Where(t => t.IsAssignableFrom(typeof(Hub))).ToArray();
-            Type[] hubs = types.Where(t => t.IsAssignableTo(typeof(Hub))).ToArray();
+            Type[] hubs = types.Where(t => t.BaseType?.FullName?.Contains("Microsoft.AspNetCore.SignalR.Hub") ?? false).ToArray();
 
             var document = generator.GenerateForHubsAsync(new Dictionary<string, Type>(hubs.Select(t => new KeyValuePair<string, Type>(t.Name, t)))).Result;
 
